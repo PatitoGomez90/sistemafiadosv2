fdf0ac7366f26d756f05f20ab71e5f7eaed2f67c
Patito <jeremiasgomez990@gmail.com>
2021-01-29 17:13:48 -0300

    test

---
+++ public/js/main.js
@@ -0,0 +1,562 @@
+function soloNumeros(e){
+    var key = window.Event ? e.which : e.keyCode
+    return (key >= 45 && key <= 57)
+}//onKeyPress="return soloNumeros(event)"
+
+function changeDate(date){
+	// input: dd/mm/yyyy
+	fechaus = date.substring(6,10) + "/" + date.substring(3,5) + "/" + date.substring(0,2);
+	return fechaus;
+	// output: yyyy/mm/dd
+}
+
+function changeDate2(date){
+	// input: yyyy/mm/dd
+	fechaus = date.substring(8,10) + "/" + date.substring(5,7) + "/" + date.substring(0,4);
+	return fechaus;
+	// output: dd/mm/yyyy
+}
+
+function Numy1Punto(e, field){
+	key = e.keyCode ? e.keyCode : e.which
+	// backspace
+	if (key == 8) return true
+	// 0-9
+	if (key > 47 && key < 58) {
+	if (field.value == "") return true
+	regexp = /.[0-9]{2}$/
+	return !(regexp.test(field.value))
+	}
+	// .
+	if (key == 46) {
+	if (field.value == "") return false
+	regexp = /^[0-9]+$/
+	return regexp.test(field.value)
+	}
+	// other key
+	return false
+}//onKeyPress="return Numy1Punto(event, this)"
+
+function checkDec(el){
+	var ex = /^[0-9]+\.?[0-9]*$/;
+	if(ex.test(el.value)==false){
+	   	el.value = el.value.substring(0,el.value.length - 1);
+	}
+}//i cant remember what this does
+
+function validate(evt){
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  var regex = /[0-9]|\./;
+  if( !regex.test(key) ) {
+    theEvent.returnValue = false;
+    if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}// onKeyPress='validate(event)'
+
+function Validate5EntY2Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+            	// dos decimales
+                regexp = /[0-9]{2}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{5}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate5EntY2Dec(event,this)"
+
+function Validate3EntY3Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{3}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{3}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate3EntY3Dec(event,this)"
+
+function Validate8EntY2Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{2}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{8}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate8EntY2Dec(event,this)"
+
+function Validate4EntY2Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{2}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{4}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate4EntY2Dec(event,this)"
+
+function Validate3EntY6Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{6}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{3}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate3EntY6Dec(event,this)"
+
+function validarCuit(cuit) { 
+    if(cuit.length != 11) {
+        return false;
+    }
+
+    var acumulado   = 0;
+    var digitos     = cuit.split("");
+    var digito      = digitos.pop();
+
+    for(var i = 0; i < digitos.length; i++) {
+        acumulado += digitos[9 - i] * (2 + (i % 6));
+    }
+
+    var verif = 11 - (acumulado % 11);
+    if(verif == 11) {
+        verif = 0;
+    } else if(verif == 10) {
+        verif = 9;
+    }
+
+    return digito == verif;
+}
+
+function generateTodayDateDMY(){
+    var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+
+    if (day < 10) { day = '0' + day }
+    if (month < 10) { month = '0' + month }
+
+    today = day+'/'+month+'/'+today.getFullYear();
+    return today;
+}
+
+function Validate10EntY2Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{2}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{10}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate10EntY2Dec(event,this)"
+
+// PARA EL SWIG CUANDO SE USA CON AJAX EN LAS VISTAS
+function parseSwig(input, data){
+    var output = swig.render(input, { locals: {
+        data: data
+    }});
+
+    return output;
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function Validate12EntY2Dec(e, field){
+    key = e.keyCode ? e.keyCode : e.which
+    // backspace
+    if (key == 8) return true
+ 
+    // 0-9 a partir del .decimal  
+    if (field.value != "") {
+        if ((field.value.indexOf(".")) > 0) {
+            //si tiene un punto valida dos digitos en la parte decimal
+            if (key > 47 && key < 58) {
+                if (field.value == "") return true
+                //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+                // dos decimales
+                regexp = /[0-9]{2}$/
+                return !(regexp.test(field.value))
+            }
+        }
+    }
+    // 0-9 
+    if (key > 47 && key < 58) {
+        if (field.value == "") return true
+        // 10 enteros?
+        regexp = /[0-9]{12}/
+        return !(regexp.test(field.value))
+    }
+    // .
+    if (key == 46) {
+        if (field.value == "") return false
+        regexp = /^[0-9]+$/
+        return regexp.test(field.value)
+    }
+    // other key
+    return false
+}//onKeyPress="return Validate12EntY2Dec(event,this)"
+
+function Validate7EntY2Dec_Neg(e, field) {// ^[+-]?[0-9]{1,9}(?:\.[0-9]{1,2})?$
+   key = e.keyCode ? e.keyCode : e.which
+   // backspace
+   if (key == 8) return true
+
+   // 0-9 a partir del .decimal  
+   if (field.value != "") {
+       if ((field.value.indexOf(".")) > 0) {
+           //si tiene un punto valida dos digitos en la parte decimal
+           if (key > 47 && key < 58) {
+               if (field.value == "") return true
+               //regexp = /[0-9]{1,10}[\.][0-9]{1,3}$/
+               // dos decimales
+               regexp = /[0-9]{2}$/
+               return !(regexp.test(field.value))
+           }
+       }
+   }
+   // 0-9 
+   if (key > 47 && key < 58) {
+       if (field.value == "") return true
+       // 10 enteros?
+       regexp = /[0-9]{7}/
+       return !(regexp.test(field.value))
+   }
+   // .
+   if (key == 46) {
+       if (field.value == "") return false
+       regexp = /^[+-]?[0-9]{7}\\.[0-9]{2}$/
+       return regexp.test(field.value)
+   }
+   // other key
+   return false
+}//onkeypress="return Validate7EntY2Dec_Neg(event,this)"
+
+// var numeroALetras = (function() {
+
+    function Unidades(num){
+        switch(num)
+        {
+            case 1: return 'UN';
+            case 2: return 'DOS';
+            case 3: return 'TRES';
+            case 4: return 'CUATRO';
+            case 5: return 'CINCO';
+            case 6: return 'SEIS';
+            case 7: return 'SIETE';
+            case 8: return 'OCHO';
+            case 9: return 'NUEVE';
+        }
+
+        return '';
+    }//Unidades()
+
+    function Decenas(num){
+
+        let decena = Math.floor(num/10);
+        let unidad = num - (decena * 10);
+
+        switch(decena)
+        {
+            case 1:
+                switch(unidad)
+                {
+                    case 0: return 'DIEZ';
+                    case 1: return 'ONCE';
+                    case 2: return 'DOCE';
+                    case 3: return 'TRECE';
+                    case 4: return 'CATORCE';
+                    case 5: return 'QUINCE';
+                    default: return 'DIECI' + Unidades(unidad);
+                }
+            case 2:
+                switch(unidad)
+                {
+                    case 0: return 'VEINTE';
+                    default: return 'VEINTI' + Unidades(unidad);
+                }
+            case 3: return DecenasY('TREINTA', unidad);
+            case 4: return DecenasY('CUARENTA', unidad);
+            case 5: return DecenasY('CINCUENTA', unidad);
+            case 6: return DecenasY('SESENTA', unidad);
+            case 7: return DecenasY('SETENTA', unidad);
+            case 8: return DecenasY('OCHENTA', unidad);
+            case 9: return DecenasY('NOVENTA', unidad);
+            case 0: return Unidades(unidad);
+        }
+    }//Unidades()
+
+    function DecenasY(strSin, numUnidades) {
+        if (numUnidades > 0)
+            return strSin + ' Y ' + Unidades(numUnidades)
+
+        return strSin;
+    }//DecenasY()
+
+    function Centenas(num) {
+        let centenas = Math.floor(num / 100);
+        let decenas = num - (centenas * 100);
+
+        switch(centenas)
+        {
+            case 1:
+                if (decenas > 0)
+                    return 'CIENTO ' + Decenas(decenas);
+                return 'CIEN';
+            case 2: return 'DOSCIENTOS ' + Decenas(decenas);
+            case 3: return 'TRESCIENTOS ' + Decenas(decenas);
+            case 4: return 'CUATROCIENTOS ' + Decenas(decenas);
+            case 5: return 'QUINIENTOS ' + Decenas(decenas);
+            case 6: return 'SEISCIENTOS ' + Decenas(decenas);
+            case 7: return 'SETECIENTOS ' + Decenas(decenas);
+            case 8: return 'OCHOCIENTOS ' + Decenas(decenas);
+            case 9: return 'NOVECIENTOS ' + Decenas(decenas);
+        }
+
+        return Decenas(decenas);
+    }//Centenas()
+
+    function Seccion(num, divisor, strSingular, strPlural) {
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+
+        let letras = '';
+
+        if (cientos > 0)
+            if (cientos > 1)
+                letras = Centenas(cientos) + ' ' + strPlural;
+            else
+                letras = strSingular;
+
+        if (resto > 0)
+            letras += '';
+
+        return letras;
+    }//Seccion()
+
+    function Miles(num) {
+        let divisor = 1000;
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+
+        let strMiles = Seccion(num, divisor, 'UN MIL', 'MIL ');
+        let strCentenas = Centenas(resto);
+
+        if(strMiles == '')
+            return strCentenas;
+
+        return strMiles + '' + strCentenas;
+    }//Miles()
+
+    function Millones(num) {
+        let divisor = 1000000;
+        let cientos = Math.floor(num / divisor)
+        let resto = num - (cientos * divisor)
+        console.log(resto)
+        if(resto == 0){
+          var millones = 'MILLONES DE';
+          var millon = 'UN MILLON DE';
+        }else{
+          var millones = 'MILLONES';
+          var millon = 'UN MILLON';
+        }
+
+
+        let strMillones = Seccion(num, divisor, millon, millones);
+        let strMiles = Miles(resto);
+        //let strMillones = Seccion(num, divisor, 'UN MILLON DE', 'MILLONES ');
+        if(strMillones == '')
+            return strMiles;
+
+        return strMillones + ' ' + strMiles;
+    }//Millones()
+
+    function NumeroALetras(num, currency) {
+        currency = currency || {};
+        let data = {
+            numero: num,
+            enteros: Math.floor(num),
+            centavos: (((Math.round(num * 100)) - (Math.floor(num) * 100))),
+            letrasCentavos: '',
+            letrasMonedaPlural: currency.plural || 'PESOS CHILENOS',//'PESOS', 'Dólares', 'Bolívares', 'etcs'
+            letrasMonedaSingular: currency.singular || 'PESO CHILENO', //'PESO', 'Dólar', 'Bolivar', 'etc'
+            letrasMonedaCentavoPlural: currency.centPlural || 'CHIQUI PESOS CHILENOS',
+            letrasMonedaCentavoSingular: currency.centSingular || 'CHIQUI PESO CHILENO'
+        };
+
+        if (data.centavos > 0) {
+            data.letrasCentavos = 'CON ' + (function () {
+                    if (data.centavos == 1)
+                        return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoSingular;
+                    else
+                        return Millones(data.centavos) + ' ' + data.letrasMonedaCentavoPlural;
+                })();
+        };
+
+        if(data.enteros == 0)
+            return 'CERO ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+        if (data.enteros == 1)
+            return Millones(data.enteros) + ' ' + data.letrasMonedaSingular + ' ' + data.letrasCentavos;
+        else
+            return Millones(data.enteros) + ' ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos;
+    };
+
+// })();
+
+// Modo de uso: 500,34 USD
+// numeroALetras(500.34, {
+//   plural: 'Pesos',
+//   singular: 'Peso',
+//   centPlural: 'centavos',
+//   centSingular: 'centavo'
+// });
---
+++ public/js/utils.js
@@ -0,0 +1,109 @@
+module.exports = {
+	generateTodayDateYMD: generateTodayDateYMD,
+	changeDate: changeDate,
+    changeDate2: changeDate2,
+    changeDate3: changeDate3,
+    MaysPrimera: MaysPrimera,
+    Excel_Export: Excel_Export,
+    pad: pad
+}
+
+function generateTodayDateYMD () {
+    var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+
+    if (day < 10) { day = '0' + day }
+    if (month < 10) { month = '0' + month }
+
+    today = today.getFullYear() + '-' + month + '-' + day;
+    return today;
+}
+
+function changeDate(date){
+	// input: dd/mm/yyyy
+	fechaus = date.substring(6,10) + "-" + date.substring(3,5) + "-" + date.substring(0,2);
+	return fechaus;
+	// output: yyyy-mm-dd
+}
+
+function changeDate2(date){
+    // input: dd/mm/yyyy
+    fechaus = date.substring(6,10) + "/" + date.substring(3,5) + "/" + date.substring(0,2);
+    return fechaus;
+    // output: yyyy-mm-dd
+}
+
+function changeDate3(date){
+    // input: dd/mm/yyyy
+    fechaus = date.substring(6,10) + "-" + date.substring(3,5) + "-" + date.substring(0,2);
+    return fechaus;
+    // output: yyyy-mm-dd
+}
+
+//mayus primera letra
+function MaysPrimera(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function Excel_Export(array){
+    console.log("go!")
+    //var cellData = "Give me something to believe";
+
+    var conf = {};
+
+    //este tiene una url acá pero en el server es otra....
+    conf.stylesXmlFile = "D:/Proyectos/Pyme.JS/style.xml";
+        // conf.stylesXmlFile = "C:/Users/Administrador/Documents/Proyectos/Maresa/style.xml";
+
+    //iterar el array e ir agregando cada uno de los json a conf.cols
+    //traer desde la base dedatos, cada uno de los nombres de los campos con mayuscula al primero
+    conf.cols = [{caption:'Codigo', type:'string'},
+    {caption:'Denominacion', type:'string'},
+    {caption:'Imputable', type:'string'},
+    {caption:'Ajustable', type:'string'},
+    {caption:'Nivel', type:'string'}];
+
+    var arrPlan = [];
+
+    for (var x = 0 ; x < plandecuentas.length ; x++){
+        //iterar adentro de este for, de nuevo, cada uno de los json e ir agregandolos a "plan"(en este ejemplo)
+        
+        var cuenta = plandecuentas[x].cuenta;
+        var nombre = plandecuentas[x].nombre;
+        var impu = plandecuentas[x].impu;
+        if (impu == 'S')
+            impu = 'Si';
+        else
+            impu = 'No';
+        var ajus = plandecuentas[x].ajus;
+        if (ajus == 'S')
+            ajus = 'Si';
+        else
+            ajus = 'No';
+        var nivel = plandecuentas[x].nivel;
+
+        var plan = [];
+
+        plan.push(cuenta);
+        plan.push(nombre);
+        plan.push(impu);
+        plan.push(ajus);
+        plan.push(nivel);
+
+        arrPlan.push(plan);
+    }
+
+    conf.rows = arrPlan;
+    var result = nodeExcel.execute(conf);
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+    res.setHeader("Content-Disposition", "attachment; filename=" + "PlanDeCuentas.xlsx");
+    res.end(result, 'binary');
+    console.log("finished")
+}
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
